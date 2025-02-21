a161296e11a57af5c7debcd874f1e721a4a2fdb1
Joni Roine <joni.roine@outlook.com>
2025-02-21 13:52:56 +0200

    Add middleware to handle authentication checks

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SumServiceService } from './sum-service/sum-service.service';
-import { SaynameModule } from './sayname/sayname.module'; // 🟢 Oikein
+import { SaynameModule } from './sayname/sayname.module';
+import { AuthchecMiddleware } from './middleware/authchec.middleware';
 
 @Module({
   imports: [SaynameModule],
   controllers: [AppController],
   providers: [AppService, SumServiceService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthchecMiddleware)
+      .forRoutes({ path: '', method: RequestMethod.ALL });
+  }
+}
