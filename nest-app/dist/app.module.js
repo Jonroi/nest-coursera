1259beb95887efc69aee9fc7ce424174e9209224
Joni Roine <joni.roine@outlook.com>
2025-02-21 13:52:38 +0200

    Add AuthchecMiddleware configuration in AppModule

--- nest-app/dist/app.module.js
+++ nest-app/dist/app.module.js
@@ -12,7 +12,13 @@ const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const sum_service_service_1 = require("./sum-service/sum-service.service");
 const sayname_module_1 = require("./sayname/sayname.module");
+const authchec_middleware_1 = require("./middleware/authchec.middleware");
 let AppModule = class AppModule {
+    configure(consumer) {
+        consumer
+            .apply(authchec_middleware_1.AuthchecMiddleware)
+            .forRoutes({ path: '', method: common_1.RequestMethod.ALL });
+    }
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
