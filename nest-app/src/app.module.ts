f68aacd0179636921f63ba80643d1dd9cfc6a009
Joni Roine <joni.roine@outlook.com>
2025-02-21 11:37:33 +0200

    Add SaynameModule to AppModule imports
    This change integrates the SaynameModule into the application by adding it to the AppModule imports. This ensures the module's functionality is available across the app.

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SumServiceService } from './sum-service/sum-service.service';
+import { SaynameModule } from './sayname/sayname.module'; // 🟢 Oikein
 
 @Module({
-  imports: [],
+  imports: [SaynameModule],
   controllers: [AppController],
   providers: [AppService, SumServiceService],
 })
